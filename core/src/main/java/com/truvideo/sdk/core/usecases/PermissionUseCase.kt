148dff206d12ac43ab50f1c29c25104ad25d8263
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2024-05-23 18:05:02 +0000

    Update 71.1.1-beta.86

---
+++ core/src/main/java/com/truvideo/sdk/core/usecases/PermissionUseCase.kt
@@ -0,0 +1,69 @@
+package com.truvideo.sdk.core.usecases
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.activity.ComponentActivity
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.truvideo.sdk.core.interfaces.TruvideoSdkCallback
+import kotlinx.coroutines.CancellableContinuation
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+
+internal class PermissionUseCase {
+
+    private var requesters = mutableMapOf<ComponentActivity, TruvideoSdkPermissionRequester>()
+
+    fun init(activity: ComponentActivity): TruvideoSdkPermissionRequester {
+        var requester = requesters[activity]
+        if (requester == null) {
+            requester = TruvideoSdkPermissionRequester(activity)
+            requesters[activity] = requester
+        }
+
+        return requester
+    }
+}
+
+class TruvideoSdkPermissionRequester(private val activity: ComponentActivity) {
+    private val scope = CoroutineScope(Dispatchers.Main)
+    private var continuation: CancellableContinuation<Boolean>? = null
+    private val startForResult: ActivityResultLauncher<Array<String>> =
+        activity.registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+            val allGranted = it.values.all { permission -> permission }
+            continuation?.resumeWith(Result.success(allGranted))
+        }
+
+    suspend fun askReadStoragePermission(): Boolean {
+        val permissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            listOf(Manifest.permission.READ_MEDIA_VIDEO, Manifest.permission.READ_MEDIA_IMAGES)
+        } else {
+            listOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+        startForResult.launch(permissions.toTypedArray())
+        return suspendCancellableCoroutine { continuation = it }
+    }
+
+    fun askReadStoragePermission(callback: TruvideoSdkCallback<Boolean>) {
+        scope.launch {
+            val result = askReadStoragePermission()
+            callback.onComplete(result)
+        }
+    }
+
+    fun hasReadStoragePermission(): Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            val videoPermission = ContextCompat.checkSelfPermission(activity, Manifest.permission.READ_MEDIA_VIDEO)
+            val imagePermission = ContextCompat.checkSelfPermission(activity, Manifest.permission.READ_MEDIA_IMAGES)
+            return videoPermission == PackageManager.PERMISSION_GRANTED && imagePermission == PackageManager.PERMISSION_GRANTED
+        } else {
+            val storagePermission = ContextCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE)
+            return storagePermission == PackageManager.PERMISSION_GRANTED
+        }
+    }
+}
