3732ed76c8e62aabbdc68c08a34fda83cf5bb595
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2024-10-29 16:29:34 +0000

    Update 76.2.2-beta.119

---
+++ core/src/main/java/com/truvideo/sdk/core/TruvideoSdk.kt
@@ -0,0 +1,8 @@
+@file:JvmName("TruvideoSdk")
+
+package com.truvideo.sdk.core
+
+import com.truvideo.sdk.core.interfaces.TruvideoSdk
+
+@get:JvmName("getInstance")
+lateinit var TruvideoSdk: TruvideoSdk
---
+++ core/src/main/java/com/truvideo/sdk/core/examples/KotlinAuthExample.kt
@@ -0,0 +1,44 @@
+package com.truvideo.sdk.core.examples
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.truvideo.sdk.core.TruvideoSdk
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class KotlinAuthExample : ComponentActivity() {
+
+    private val scope = CoroutineScope(Dispatchers.Main)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        scope.launch {
+            authenticate()
+        }
+    }
+
+    private suspend fun authenticate() {
+        try {
+            val isAuthenticated = TruvideoSdk.isAuthenticated()
+            val isAuthenticationExpired = TruvideoSdk.isAuthenticationExpired()
+            if (!isAuthenticated || isAuthenticationExpired) {
+                val apiKey = "your-api-key"
+                val payload = TruvideoSdk.generatePayload()
+                val signature = "your-signature"
+                TruvideoSdk.authenticate(
+                    apiKey = apiKey,
+                    payload = payload,
+                    signature = signature
+                )
+            }
+
+            TruvideoSdk.initAuthentication()
+
+            // Authentication ready
+            // Truvideo SDK its ready to be used
+        } catch (exception: Exception) {
+            exception.printStackTrace()
+            // Handle error
+        }
+    }
+}
